825c4803b3495234a8ef9a3697525b7ab2330226
kolbeee <80353585+kolbeee@users.noreply.github.com>
2021-03-09 11:26:57 -0700

    Add files via upload

---
+++ HelloworldProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HelloWorldProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logging in as TRIAL_USER!");
+            Console.WriteLine("This is not right.. You need admin rights to run this program!");
+            Console.WriteLine("Please Make sure your are in admin to delete system32!");
+            Console.WriteLine("Admin Approved!");
+            Console.WriteLine("Error downloading backup files!");
+            Console.WriteLine("Product key invalid!");
+            Console.WriteLine("Current Product key:432-TRIAL-VERISON-OEM-0001 Is no longer valid! Please buy the program");
+            Console.WriteLine("Press any button to quit the program!");
+            Console.WriteLine("Found OEM Key!!");
+            Console.WriteLine("Logging in as Cracked_User!");
+            Console.WriteLine("App ownership Not verified! Please relanch the client");
+            Console.WriteLine("Not a valid session Trying to reconnect!");
+            Console.WriteLine("YOU HAVE BEEN BANNED FROM SERVER 1.56.643.99.22!");
+            Console.WriteLine("Reason:");
+            Console.WriteLine("Watchdog Unknown username! Cracked Client!");
+            Console.WriteLine("Please don't use clients like TLancher, badlion, lunar client, or others!");
+            Console.WriteLine("If you would like to still use a client like this please send us a donation, and you will get an OTP code sent to your phone!");
+            Console.WriteLine("Thank you your OTP Code is 34412556, 1. DO NOT SHARE WITH ANYONE! 2. Please type your code into the filed of the game client!");
+            Console.WriteLine("Thank you! You may now play the game!");
+            Console.WriteLine("Cracked_User has connected to the server!");
+            Console.WriteLine("Hacked!");
+            Console.WriteLine("AES BYPASS ENABLED: true");
+            Console.WriteLine("Errors are red my screen is blue i think i !");
+        }
+    }
+}
